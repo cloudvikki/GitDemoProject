5bad7f81fcefacb892c8051f12573d492d66cfb7
Vikki <aishwaryavikki@gmail.com>
2023-04-05 12:37:50 +0530

    First Commit

--- src/GitCheck/GitFirst.java
+++ src/GitCheck/GitFirst.java
@@ -1,10 +1,25 @@
 package GitCheck;
 
+
+
 public class GitFirst {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int f=12;
+		int j=1;
+		GitFirst xyz=new GitFirst();
+		int v=xyz.minus(f, j);
+				
+		System.out.println(v);
 
 	}
+	
+	public int minus (int a,int b) {
+		int c = a*b;
+		return c;
+		
+		
+	}
+	
 
 }
